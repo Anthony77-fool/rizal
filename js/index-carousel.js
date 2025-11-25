5cfbac97adaa9e1a9af8306971bffe8b390707a3
Anthony77-fool <sabadomarckanthony@gmail.com>
2025-11-26 03:52:29 +0800

    Fixes Animations

--- js/index-carousel.js
+++ js/index-carousel.js
@@ -63,3 +63,76 @@ $(document).ready(function(){
         slideInterval = setInterval(nextSlide, 3000);
     });
 });
+
+$(document).ready(function () {
+
+    const fullText = "Why Study Rizal?";
+    const loopPart = "zal?";   // letters to erase and retype in loop
+    const loopStartIndex = fullText.indexOf(loopPart);
+    const $heading = $(".mandate-text h2");
+
+    let animatedOnce = false;
+
+    function typeFullText(callback) {
+        let i = 0;
+        $heading.text("");
+        let interval = setInterval(() => {
+            $heading.text(fullText.substring(0, i + 1));
+            i++;
+            if (i === fullText.length) {
+                clearInterval(interval);
+                if (callback) callback();
+            }
+        }, 70);
+    }
+
+    function startLoop() {
+        let erasing = false;
+        let i = fullText.length;
+
+        setInterval(() => {
+            if (!erasing) {
+                // ERASE loop part
+                if (i > loopStartIndex) {
+                    i--;
+                    $heading.text(fullText.substring(0, i));
+                } else {
+                    erasing = true;
+                }
+            } else {
+                // TYPE loop part again
+                if (i < fullText.length) {
+                    i++;
+                    $heading.text(fullText.substring(0, i));
+                } else {
+                    erasing = false;
+                }
+            }
+        }, 300);
+    }
+
+    // Cursor append
+    $("<span class='typing-cursor'></span>").insertAfter($heading);
+
+    // Observer to trigger animation on scroll
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting && !animatedOnce) {
+                animatedOnce = true;
+
+                // Type full text first
+                typeFullText(() => {
+                    // Reveal paragraphs after full heading
+                    $(".mandate-text p").css("visibility", "visible");
+
+                    // Start looping animation
+                    startLoop();
+                });
+
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.3 });
+
+    observer.observe(document.querySelector('.mandate-section'));
+});
